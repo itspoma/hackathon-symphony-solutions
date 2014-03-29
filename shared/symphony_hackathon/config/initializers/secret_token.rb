ae3d3f436cfdd31dea1088db085741d07e9c6968
user.name <your.email@com>
2014-03-29 13:39:37 +0200

    Project initialized

---
+++ shared/symphony_hackathon/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SymphonyHackathon::Application.config.secret_key_base = '0fe718a83cf4b68fcb97583064cb79e8a1fe0b9ceebe0efed521cb74a76f2e71587950a536f1fd45d12b8faf282200cbf59f6526a2c7676e2f60c128edf04694'
